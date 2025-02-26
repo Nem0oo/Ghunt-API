96242fa81a40bc3c50e4f401c64891b45dbb68b5
Nem0oo <gcourtot@gmail.com>
2025-02-26 05:20:02 +0000

    Add pseudo security

--- api/api.py
+++ api/api.py
@@ -1,14 +1,18 @@
 from fastapi import FastAPI
+from pathlib import Path
 import subprocess
 
 app = FastAPI()
 
 @app.get("/ghunt")
-def run_ghunt(command: str, params: str):
-    try:
-        result = subprocess.run([
-            "docker", "exec", "ghunt", "ghunt", command, params,
-        ], capture_output=True, text=True)
-        return {"output": result.stdout, "error": result.stderr}
-    except Exception as e:
-        return {"error": str(e)}
+def run_ghunt(command: str, params: str, key: str):
+    secret = Path('api_data/key.txt').read_text()
+    if key == secret:
+        try:
+            result = subprocess.run([
+                "docker", "exec", "ghunt", "ghunt", command, params,
+            ], capture_output=True, text=True)
+            return {"output": result.stdout, "error": result.stderr}
+        except Exception as e:
+            return {"error": str(e)}
+    return {"error":"Invalid key"}
