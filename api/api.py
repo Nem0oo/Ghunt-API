937bf4ad511dc2102b42ef9c4f3232d62b5c2aa7
Nem0oo <gcourtot@gmail.com>
2025-02-28 04:34:49 +0000

    Add json to result and to local dir. Edited gitignore

--- api/api.py
+++ api/api.py
@@ -1,18 +1,21 @@
 from fastapi import FastAPI
 from pathlib import Path
 import subprocess
+import time
 
 app = FastAPI()
 
 @app.get("/ghunt")
 def run_ghunt(command: str, params: str, key: str):
     secret = Path('api_data/key.txt').read_text()
+    jsonFileName = Path('/acount_info/' + time.strftime("%Y%m%d-%H%M%S") + params.replace("@gmail.com",".json"))
     if key == secret:
         try:
             result = subprocess.run([
-                "docker", "exec", "ghunt", "ghunt", command, params,
+                "docker", "exec", "ghunt", "ghunt", command, params, "--json", jsonFileName
             ], capture_output=True, text=True)
-            return {"output": result.stdout, "error": result.stderr}
+            jsonContent = Path(jsonFileName).read_text()
+            return {"output": {"text" : result.stdout, "json" : jsonContent}, "error": result.stderr}
         except Exception as e:
             return {"error": str(e)}
     return {"error":"Invalid key"}
